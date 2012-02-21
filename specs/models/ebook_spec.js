498407b6543251cc7c6b8f902a88c5aba75802a7
Matthew Robertson <matthewr@evidentpoint.com>
2012-02-21 09:29:14 -0800

    included jasmine in the project and wrote a dummy spec

---
+++ specs/models/ebook_spec.js
@@ -0,0 +1,13 @@
+describe("EBook", function() {
+
+	var testVal;
+	
+	beforeEach(function() {
+		testVal = true;
+	});
+
+	it("should pass this spec", function() {
+		expect(testVal).toBeTruthy();
+	});
+
+});
