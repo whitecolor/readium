f13f9313e452ed332e10b88329ac6c8a0982d3c6
Matthew Robertson <matthewrobertson03@gmail.com>
2012-03-05 17:30:48 -0800

    added some specs for toggling two up logic

--- spec/javascripts/models/ebook_spec.js
+++ spec/javascripts/models/ebook_spec.js
@@ -13,7 +13,6 @@ describe("ebook", function() {
 		var ebook;
 		
 		beforeEach(function() {
-			
 			spyOn(Readium.Models, "PackageDocument").andReturn(pacDocStub);
 		});
 
@@ -56,26 +55,72 @@ describe("ebook", function() {
 			ebook = new Readium.Models.EbookBase;
 		});
 
-		it('sets the current page to 1 by default', function() {
+		it('sets the current page to [1] by default', function() {
 			ebook = new Readium.Models.EbookBase({"package_doc_path": "banana"});
-			expect(ebook.get("current_page")).toEqual(1);
+			expect(ebook.get("current_page")[0]).toEqual(1);
 		});
 
 		it("increments the page number if there are more pages", function() {
 			ebook.set({"num_pages": 10});
 			ebook.nextPage();
-			expect(ebook.get("current_page")).toEqual(2);
+			expect(ebook.get("current_page")[0]).toEqual(2);
+		});
+
+		it("increments the page number if there are more pages", function() {
+			ebook.set({"num_pages": 10});
+			ebook.nextPage();
+			expect(ebook.get("current_page")[0]).toEqual(2);
 		});
 
 		it('increments the section of package doc if there are no more pages', function() {
 			spyOn(pacDocStub, "goToNextSection").andReturn(true);
 			ebook.set({"num_pages": 10});
-			ebook.set({"current_page": 10});
+			ebook.set({"current_page": [10]});
 			ebook.nextPage();
-			expect(pacDocStub.goToNextSection).toHaveBeenCalled()
-			expect(ebook.get("current_page")).toEqual(1);
+			expect(pacDocStub.goToNextSection).toHaveBeenCalled();		
 		})
 
-	});
+	})
+
+	describe("paginating", function() {
+
+		var ebook;
+		
+		beforeEach(function() {
+			pacDocStub.goToNextSection = function() {}
+			spyOn(Readium.Models, "PackageDocument").andReturn(pacDocStub);
+			ebook = new Readium.Models.EbookBase;
+		});
+
+		it("sets the length of the current_page array to two in two up", function() {
+			ebook.set({"num_pages": 10});
+			ebook.toggleTwoUp();
+			expect(ebook.get("current_page").length).toEqual(2);
+		});
+
+		it("sets the pages to [0,1] when toggling on the first page", function() {
+			// edge case
+			ebook.set({"num_pages": 10, "current_page": [1]});
+			ebook.toggleTwoUp();
+			expect(ebook.get("current_page")[0]).toEqual(0);
+			expect(ebook.get("current_page")[1]).toEqual(1);
+		});
+
+		it("sets the pages to [2,3] when toggling on page 1 or 3", function() {
+			ebook.set({"num_pages": 10, "current_page": [2]});
+			ebook.toggleTwoUp();
+			expect(ebook.get("current_page")[0]).toEqual(2);
+			expect(ebook.get("current_page")[1]).toEqual(3);
+
+			ebook.toggleTwoUp();
+			ebook.set({"current_page": [3]});
+
+			ebook.toggleTwoUp();
+			expect(ebook.get("current_page")[0]).toEqual(2);
+			expect(ebook.get("current_page")[1]).toEqual(3);
+
+		});
+
+	})
 
 });
