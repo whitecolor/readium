57005d2172df0aa758e9d1446294831cc60ef508
Matthew Robertson <matthewrobertson03@gmail.com>
2012-05-18 16:44:29 -0700

    styled the options modal

--- scripts/views/toolbar_view.js
+++ scripts/views/toolbar_view.js
@@ -3,26 +3,48 @@ Readium.Views.ToolbarView = Backbone.View.extend({
 	el: "#toolbar-el",
 
 	initialize: function() {
-		this.model.on("change:toolbar_visible", this.render, this);
+		this.model.on("change:toolbar_visible", this.renderBarVibility, this);
+		this.model.on("change:full_screen", this.renderFullScreen, this);
 	},
 
 	render: function() {
-		console.log("toolbar render called")
-		this.$('#show-toolbar-button').toggle( !this.model.get("toolbar_visible") );
-		this.$('#top-bar').toggle( this.model.get("toolbar_visible") );
+		this.renderBarVibility();
+		this.renderFullScreen();
+		return this;
+	},
+
+	renderBarVibility: function() {
+		var visible = this.model.get("toolbar_visible");
+		this.$('#show-toolbar-button').toggle( !visible );
+		this.$('#top-bar').toggle( visible );
+		return this;
+	},
+
+	renderFullScreen: function() {
+		var isFs = this.model.get("full_screen");
+		this.$("#go-to-fs-ico").toggle( !isFs );
+		this.$("#leave-fs-ico").toggle( isFs );
 		return this;
 	},
 
 	events: {
 		"click #hide-toolbar-button": "hide_toolbar",
 		"click #show-toolbar-button": "show_toolbar",
+		"click #fs-toggle-btn": "toggle_fs"
 	},
 
-	show_toolbar: function() {
+	show_toolbar: function(e) {
+		e.preventDefault();
 		this.model.set("toolbar_visible", true);
 	},
 
-	hide_toolbar: function() {
+	hide_toolbar: function(e) {
+		e.preventDefault();
 		this.model.set("toolbar_visible", false);
+	},
+
+	toggle_fs: function(e) {
+		e.preventDefault();
+		this.model.toggleFullScreen();
 	}
 });
