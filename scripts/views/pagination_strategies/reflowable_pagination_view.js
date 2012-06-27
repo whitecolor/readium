864300cfbe0399c1f44eab8e7abbb4b404292b44
Matthew <matthewrobertson03@gmail.com>
2012-06-26 23:42:29 -0700

    fix for issue #142 - url fragment navigation for rtl content

--- scripts/views/pagination_strategies/reflowable_pagination_view.js
+++ scripts/views/pagination_strategies/reflowable_pagination_view.js
@@ -267,6 +267,7 @@ Readium.Views.ReflowablePaginationView = Readium.Views.PaginationViewBase.extend
 	},
 
 	getElemPageNumber: function(elem) {
+
 		var rects, shift;
 		rects = elem.getClientRects();
 		if(!rects || rects.length < 1) {
@@ -274,6 +275,15 @@ Readium.Views.ReflowablePaginationView = Readium.Views.PaginationViewBase.extend
 			return -1;
 		}
 		shift = rects[0][this.offset_dir];
+
+		// `clientRects` are relative to the top left corner of the frame, but
+		// for right to left we actually need to measure relative to right edge
+		// of the frame
+		if(this.offset_dir === "right") {
+			// the right edge is exactly `this.page_width` pixels from the right 
+			// edge
+			shift = this.page_width - shift;
+		}
 		// less the amount we already shifted to get to cp
 		shift -= parseInt(this.getBody().style[this.offset_dir], 10); 
 		return Math.ceil( shift / (this.page_width + this.gap_width) );
